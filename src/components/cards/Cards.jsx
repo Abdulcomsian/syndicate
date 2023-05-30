49209cb1801adb1679633c96e0e48634226b1958
Abdul Basit <a.basit@accrualhub.com>
2023-05-30 17:44:43 +0500

    Landing page developed

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,57 +1,65 @@
-import React, { useState } from 'react';
-import'./cards.css';
-import visa from '../../visa-logo.png';
-import mastercard from '../../Mastercard.png';
-import shape from '../../Shape.png';
-import western from '../../Western Union Logo Vector 1.png'
+import React, { useState } from "react";
+import "./cards.css";
+import visa from "../../visa-logo.png";
+import mastercard from "../../Mastercard.png";
+import shape from "../../Shape.png";
+import western from "../../Western Union Logo Vector 1.png";
 
 const Cards = () => {
-
-        // const [cardss]= useState([
-        //     {
-        //         title: 'card-1',
-        //         text : 'lorewm afas afsaf afasf',
-        //         image : "visa"
-        //     }
-        // ])
-
-
-
+  // const [cardss]= useState([
+  //     {
+  //         title: 'card-1',
+  //         text : 'lorewm afas afsaf afasf',
+  //         image : "visa"
+  //     }
+  // ])
 
   return (
     <>
-        <div className="container-fluid cards pt-5 pb-5">
+      <div className="container-fluid cards pt-5 pb-5">
+        <div className="container">
+          <div className="row mt-4">
+            <div className="cards-heading">
+              <p>Metodi Plačanja</p>
+            </div>
+            <div className="card-para">
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Ut elementum sit
+                ultricies consectetur
+              </p>
+            </div>
+          </div>
 
-            <div className="container">
-                <div className="row mt-4">
-                    <div className="cards-heading">
-                        <p>Metodi Plačanja</p>
-                    </div>
-                    <div className="card-para">
-                        <p>Lorem ipsum dolor sit amet consectetur. Ut elementum sit ultricies consectetur</p>
-                    </div>
-                </div>
+          <div className="d-flex justify-content-center mt-3 gap-5 mb-5">
+            <div className="credit-card">
+              {" "}
+              <img src={visa} />{" "}
+            </div>
+            <div className="credit-card">
+              {" "}
+              <img src={mastercard} />{" "}
+            </div>
+            <div className="credit-card">
+              {" "}
+              <img src={shape} />{" "}
+            </div>
+            <div className="credit-card">
+              {" "}
+              <img src={western} />{" "}
+            </div>
+          </div>
 
-                <div className="row d-flex justify-content-around mt-3">
-                    <div className="col-2 credit-card"> <img src={visa}  /> </div>
-                    <div className="col-2 credit-card"> <img src={mastercard}  /> </div>
-                    <div className="col-2 credit-card"> <img src={shape} /> </div>
-                    <div className="col-2 credit-card"> <img src={western} /> </div>
-                </div>
-
-
-
-                {/* <div className="cardss">
+          {/* <div className="cardss">
                     <div className="cardd">
                         <h1> card-1 </h1>
                         <p>Lorem ipsum dolor sit amet.</p>
                         <img src={visa} alt="" />
                     </div>
                 </div> */}
-            </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
--- src/components/plans/Plan.jsx
+++ src/components/plans/Plan.jsx
@@ -1,81 +1,92 @@
-import React from 'react'
-import './plan.css';
-import vecter2 from '../../Vector2.png'
+import React from "react";
+import "./plan.css";
+import vecter2 from "../../Vector2.png";
 
 const Plan = () => {
   return (
     <>
       <div className="container-fluid plan pt-5 pb-5">
         <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="heading-2">
-              <p>Plan & Pricing </p>
-            </div>
-            <div className="para-2">
-              <p>Whether your time-saving automation needs are large or small, we’re here to help you scale.</p>
-            </div>
+          <div className="row">
+            <div className="col">
+              <div className="heading-2">
+                <p>Plan & Pricing </p>
+              </div>
+              <div className="para-2">
+                <p>
+                  Whether your time-saving automation needs are large or small,
+                  we’re here to help you scale.
+                </p>
+              </div>
 
-
-            <div className=" tabs  ">
-            <div className="rows d-lg-flex justify-content-around">
-              <div className="col-5 me-lg-5">
-                  <div className="one mt-4">
+              <div className="row justify-content-center mt-4 plan-section">
+                <div className="col-md-5 d-flex justify-content-center">
+                  <div className="one ">
                     <div className="heading-3">
-                        <h2>Explore</h2>    
+                      <h2>Explore</h2>
                     </div>
                     <div className="vector-2">
-                          <img src={vecter2} alt="" />
-                        </div> 
+                      <img src={vecter2} alt="" />
+                    </div>
                     <div className="para-3 mt-3">
                       <p>Lorem ipsum dolor sit amet</p>
-                    </div>  
+                    </div>
                     <div className="para-4">
-                      <p>Lorem ipsum dolor sit amet consectetur. Commodo ut ut in nisi.</p>
-                    </div> 
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur. Commodo ut ut in
+                        nisi.
+                      </p>
+                    </div>
                     <div className="para-5 ms-2">
                       <p>$10</p>
-                    </div> 
+                    </div>
                     <div className="para-4">
                       <p>Per month</p>
                     </div>
 
-                    <button className='btn-4' style={{color:"white"}}> Buy Standard   </button>
+                    <button className="btn-4" style={{ color: "white" }}>
+                      {" "}
+                      Buy Standard{" "}
+                    </button>
                   </div>
-              </div>
-              <div className="col-5  ms-lg-5">
-              <div className="one mt-4">
+                </div>
+                <div className="col-md-5 d-flex justify-content-center">
+                  <div className="one ">
                     <div className="heading-3">
-                        <h2>Explore</h2>    
+                      <h2>Explore</h2>
                     </div>
                     <div className="vector-2">
-                          <img src={vecter2} alt="" />
-                        </div> 
+                      <img src={vecter2} alt="" />
+                    </div>
                     <div className="para-3 mt-3">
                       <p>Lorem ipsum dolor sit amet</p>
-                    </div>  
+                    </div>
                     <div className="para-4">
-                      <p>Lorem ipsum dolor sit amet consectetur. Commodo ut ut in nisi.</p>
-                    </div> 
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur. Commodo ut ut in
+                        nisi.
+                      </p>
+                    </div>
                     <div className="para-5">
                       <p>$10</p>
-                    </div> 
+                    </div>
                     <div className="para-4">
                       <p>Per month</p>
                     </div>
 
-                    <button className='btn-4' style={{color:"white"}}> Buy Standard </button>
+                    <button className="btn-4" style={{ color: "white" }}>
+                      {" "}
+                      Buy Standard{" "}
+                    </button>
                   </div>
-              </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Plan
+export default Plan;
